1ac2e3fe6546774d2971cdf1145a3a8988363e5d
John Mogensen <jamjaws@gmail.com>
2021-09-20 21:36:55 +0200

    feat: add say random learn

---
+++ README.md
@@ -0,0 +1,7 @@
+## Getting started
+
+Read more about the JavaScript Package for Twitch Chat here [https://tmijs.com/](https://tmijs.com/)
+
+Install npm packages `npm install`
+
+Start `npm start`
--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log("stellar!")
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+    options: {
+        debug: true,
+    },
+    connection: {
+        reconnect: true,
+        secure: true,
+    },
+    identity: {
+        username: 'my_bot_name', // REPLACE
+        password: 'oauth:my_bot_token' // REPLACE
+    },
+    channels: ["sssstellar"],
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    // Ignore echoed messages.
+    if (self) return;
+
+    if (message.toLowerCase() === '!hello') {
+        // "@alca, heya!"
+        client.say(channel, `@${tags.username}, heya!`);
+    }
+});
+
+
+function sayRandomLearn() {
+    const randomLearn = learns[getRandomInt(learns.length)];
+    client.say("sssstellar", randomLearn)
+}
+
+function loop() {
+    const maxIntervalInSeconds = 60;
+    const minIntervalInSeconds = 30;
+    var randomTimeoutInSeconds = Math.round(Math.random() * (maxIntervalInSeconds - minIntervalInSeconds)) + minIntervalInSeconds;
+    const randomTimeoutInMiliSeconds = randomTimeoutInSeconds * 1000;
+    console.log(`next random learn in ${randomTimeoutInSeconds} seconds`)
+    setTimeout(function () {
+        sayRandomLearn();
+        loop();
+    }, randomTimeoutInMiliSeconds);
+};
+
+loop()
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+const learns = [
+    "Stellar is stellar!",
+    "Coding is fun!",
+    "Everyone likes tacos!"
+];
